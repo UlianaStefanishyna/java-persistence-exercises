71f4e50a84333fbdda44977153c5ace2066ae7e0
Uliana Stefanishyna <ulanka1996@gmail.com>
2019-04-10 13:56:08 +0000

    completed photo-comment-dao

--- photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
+++ photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
@@ -1,6 +1,8 @@
 package ua.procamp.dao;
 
 import ua.procamp.model.Photo;
+import ua.procamp.model.PhotoComment;
+import ua.procamp.util.OperationWrapper;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
@@ -9,34 +11,55 @@ import java.util.List;
  * Please note that you should not use auto-commit mode for your implementation.
  */
 public class PhotoDaoImpl implements PhotoDao {
-    private EntityManagerFactory entityManagerFactory;
+
+    private final OperationWrapper operationWrapper;
 
     public PhotoDaoImpl(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
+        this.operationWrapper = new OperationWrapper(entityManagerFactory);
     }
 
     @Override
     public void save(Photo photo) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        this.operationWrapper.handleVoidOperation(entityManager -> {
+            entityManager.persist(photo);
+        });
     }
 
     @Override
     public Photo findById(long id) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        validateId(id);
+        return this.operationWrapper
+                .handleReturningResultOperation(entityManager -> entityManager.find(Photo.class, id));
     }
 
     @Override
     public List<Photo> findAll() {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        return this.operationWrapper
+                .handleReturningResultOperation(entityManager -> entityManager
+                        .createQuery("select p from Photo p", Photo.class)
+                        .getResultList());
     }
 
     @Override
     public void remove(Photo photo) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        this.operationWrapper.handleVoidOperation(entityManager -> {
+            Photo managed = entityManager.merge(photo);
+            entityManager.remove(managed);
+        });
     }
 
     @Override
     public void addComment(long photoId, String comment) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        validateId(photoId);
+        this.operationWrapper.handleVoidOperation(entityManager -> {
+            Photo photo = entityManager.find(Photo.class, photoId);
+            photo.addComment(new PhotoComment(comment));
+        });
+    }
+
+    private void validateId(long id) {
+        if (id <= 0) {
+            throw new RuntimeException("Id cannot be less or equal to zero");
+        }
     }
 }
